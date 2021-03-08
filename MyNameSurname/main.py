7ec139c6d38e8bb256aae22733fe90c46a4bcee5
Barihadi <mihail.barihadi@rambler.ru>
2021-03-08 18:42:59 +0300

    Deleted trash

---
+++ MyNameSurname/main.py
@@ -0,0 +1,9 @@
+Greetings = 'Привет, мир!'
+Name = 'Михаил'
+Surname = 'Барихади'
+print(Greetings + '\n'  'Мое имя' + ' ' + Name + ' ' + Surname)
+
+bonus = input('Напиши 1 для бонуса: ')
+
+if bonus == '1':
+    print('Профессия - ученик Неровного')
